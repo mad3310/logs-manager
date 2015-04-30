37c283b24756793e46b478c3e1c9ae82aa3efe2f
zhangzeng <zhangzeng@letv.com>
2015-04-30 18:44:44 +0800

    move openssl logic from logstash to openssl

--- api/handlers/logstash.py
+++ api/handlers/logstash.py
@@ -6,9 +6,7 @@ Created on Mar 8, 2015
 
 from base import APIHandler
 from tornado_letv.tornado_basic_auth import require_basic_auth
-from tornado.web import asynchronous
 from componentNode.logstash_opers import LogstashOpers
-from componentNode.openssl import OpensslOpers
 
 
 @require_basic_auth
@@ -16,7 +14,6 @@ class Logstash_Start_Handler(APIHandler):
     
     logstash_opers = LogstashOpers()
     
-    @asynchronous
     def post(self):
         '''
         function: start node
@@ -54,32 +51,3 @@ class Logstash_Restart_Handler(APIHandler):
         self.finish(result)
 
 
-@require_basic_auth
-class Openssl_Config_Handler(APIHandler):
-    
-    openssl_opers = OpensslOpers()
-    
-    def post(self):
-        '''
-        function: reload node
-        url example: curl --user root:root -d "" "http://localhost:8888/node/reload"
-        '''
-        
-        args = self.get_all_arguments()
-        result = self.openssl_opers.config(args)
-        self.finish(result)
-
-
-@require_basic_auth
-class Openssl_Copy_Handler(APIHandler):
-    
-    openssl_opers = OpensslOpers()
-    
-    def post(self):
-        '''
-        function: reload node
-        url example: curl --user root:root -d "" "http://localhost:8888/node/reload"
-        '''
-        result = self.openssl_opers.copyssl()
-        self.finish(result)
-        
---
+++ api/handlers/openssl.py
@@ -0,0 +1,34 @@
+
+from base import APIHandler
+from tornado_letv.tornado_basic_auth import require_basic_auth
+from componentNode.openssl_opers import OpensslOpers
+
+
+@require_basic_auth
+class Openssl_Config_Handler(APIHandler):
+    
+    openssl_opers = OpensslOpers()
+    
+    def post(self):
+        '''
+        function: reload node
+        url example: curl --user root:root -d "" "http://localhost:8888/node/reload"
+        '''
+        
+        args = self.get_all_arguments()
+        result = self.openssl_opers.config(args)
+        self.finish(result)
+
+
+@require_basic_auth
+class Openssl_Copy_Handler(APIHandler):
+    
+    openssl_opers = OpensslOpers()
+    
+    def post(self):
+        '''
+        function: reload node
+        url example: curl --user root:root -d "" "http://localhost:8888/node/reload"
+        '''
+        result = self.openssl_opers.copyssl()
+        self.finish(result)
