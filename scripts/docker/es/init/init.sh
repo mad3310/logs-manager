2e5511e3c1f5f6dbbb3d6da621dc9392bf546575
denglj <dengliangju@le.com>
2016-07-01 17:59:33 +0800

    add elk together

---
+++ scripts/docker/es/init/init.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo 'do init action'
+mkdir -p /var/log/elasticsearch
+chown -R elasticsearch /var/log/elasticsearch
+chown -R elasticsearch /srv/esdata
+
+#init network
+function checkvar(){
+  if [ ! $2 ]; then
+    echo ERROR: need  $1
+    exit 1
+  fi
+}
+
+IFACE=${IFACE:-pbond0}
+
+checkvar IP $IP
+checkvar NETMASK $NETMASK
+checkvar GATEWAY $GATEWAY
+
+#network
+cat > /etc/sysconfig/network-scripts/ifcfg-$IFACE << EOF
+DEVICE=$IFACE
+ONBOOT=yes
+BOOTPROTO=static
+IPADDR=$IP
+NETMASK=$NETMASK
+GATEWAY=$GATEWAY
+EOF
+ifconfig $IFACE $IP/16
+echo 'set network successfully'
