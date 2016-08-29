784eedb9c2e70394add17d5f1a110ecfe13d5b9b
王翊仰 <wangyiyang.kk@gmail.com>
2016-08-29 18:24:42 +0800

    增加自监控，邮件发送结果

--- src/api/utils/es_monitor.py
+++ src/api/utils/es_monitor.py
@@ -5,33 +5,33 @@ import time
 from logic.zk_opers import ZkOpers
 from tornado.options import options
 
-from src.api.utils.mail import MailEgine
+from utils.mail import MailEgine
 
-from src.api.componentNode.elasticsearch_opers import ElasticsearchOpers
+from componentNode.elasticsearch_opers import ElasticsearchOpers
 
 
 class ESMonitor():
 
     @staticmethod
     def portuse(port=9200):
+        total_dic = {}
         es_opers = ElasticsearchOpers()
         node_info = es_opers.config_op.getValue(options.data_node_property, ['dataNodeIp', 'dataNodeName'])
         cluster_info = es_opers.config_op.getValue(options.cluster_property, ['clusterUUID', 'clusterName'])
         total_dic['cluster.name'] = cluster_info['clusterName']
         total_dic['node.name'] = node_info['dataNodeName']
-        subject = ""
-        body = ""
+        subject = "test subject"
+        body = "test body"
         try:
-            zkoper = ZkOpers()
-            ip = zkoper.get_self_ip()
+            ip = "127.0.0.1"
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             s.connect((ip, int(port)))
+            print "Y" * 30
             s.shutdown(2)
-            return True
+            print 1
         except:
-            MailEgine.send_normal_email(options.smtp_from_address, options.admins, subject, body)
-            return False
+            print "Z" * 30
 
-
-# node_info = self.config_op.getValue(options.data_node_property, ['dataNodeIp', 'dataNodeName'])
-# cluster_info = self.config_op.getValue(options.cluster_property, ['clusterUUID', 'clusterName'])
+            print options.smtp_from_address, ['wangyiyang <wangyiyang@le.com>'], subject, body
+            MailEgine.send_exception_email(options.smtp_from_address, ['wangyiyang <wangyiyang@le.com>'], subject, body)
+            print "W" * 30
